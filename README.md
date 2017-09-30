d7302e920fb23ab9857c576b5a851ecfe357798e
Jack Lam <jacklam718@gmail.com>
2017-09-30 21:22:28 +0800

    Rename the project

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
-# React Native App Onboarding Views
+# React Native App Onboarding Component
 
-<img src="https://raw.githubusercontent.com/jacklam718/react-native-app-onboarding/master/.github/app-onboarding.gif" width="300">
+<img src="https://raw.githubusercontent.com/jacklam718/react-native-onboarding-component/master/.github/app-onboarding.gif" width="300">
 
 # Installation
-`yarn add react-native-app-onboarding`
+`yarn add react-native-onboarding-component`
 
 # Demo
 [Try with Exponent](https://exp.host/@jacklam718/App)
 
-[Example](https://github.com/jacklam718/react-native-app-onboarding/blob/master/example/App.js)
+[Example](https://github.com/jacklam718/react-native-onboarding-component/blob/master/example/App.js)
 
 # For contributor
 Pull request are welcomed. Please follow [Airbnb JS Style Guide](https://github.com/airbnb/javascript)
--- dist/GradientBackgrounds.js
+++ dist/GradientBackgrounds.js
@@ -4,6 +4,7 @@ var _react=require('react');var _react2=_interopRequireDefault(_react);
 var _reactNative=require('react-native');function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj};}function _classCallCheck(instance,Constructor){if(!(instance instanceof Constructor)){throw new TypeError("Cannot call a class as a function");}}function _possibleConstructorReturn(self,call){if(!self){throw new ReferenceError("this hasn't been initialised - super() hasn't been called");}return call&&(typeof call==="object"||typeof call==="function")?call:self;}function _inherits(subClass,superClass){if(typeof superClass!=="function"&&superClass!==null){throw new TypeError("Super expression must either be null or a function, not "+typeof superClass);}subClass.prototype=Object.create(superClass&&superClass.prototype,{constructor:{value:subClass,enumerable:false,writable:true,configurable:true}});if(superClass)Object.setPrototypeOf?Object.setPrototypeOf(subClass,superClass):subClass.__proto__=superClass;}var _Dimensions$get=
 
 _reactNative.Dimensions.get('window'),deviceHeight=_Dimensions$get.height,deviceWidth=_Dimensions$get.width;
+var isAdroid=_reactNative.Platform.OS==='android';
 
 var styles=_reactNative.StyleSheet.create({
 container:{
@@ -38,7 +39,7 @@ scrollX:props.scrollX};return _this;
 this.props,style=_props.style,colors=_props.colors;
 console.log('colors: ',colors);
 return(
-_react2.default.createElement(_reactNative.View,{style:[styles.container,style],__source:{fileName:_jsxFileName,lineNumber:41}},
+_react2.default.createElement(_reactNative.View,{style:[styles.container,style],__source:{fileName:_jsxFileName,lineNumber:42}},
 colors.map(function(color,index){return(
 _react2.default.createElement(_reactNative.Animated.View,{
 key:'gb-'+index,
@@ -48,7 +49,7 @@ backgroundColor:color,
 zIndex:-index,
 opacity:_this2.state.scrollX.interpolate({
 inputRange:[deviceWidth*index,deviceWidth*(index+1)],
-outputRange:_reactNative.Platform.OS==='android'?[1,2]:[1,0]})}),__source:{fileName:_jsxFileName,lineNumber:43}}));})));
+outputRange:isAdroid?[1,2]:[1,0]})}),__source:{fileName:_jsxFileName,lineNumber:44}}));})));
 
 
 
